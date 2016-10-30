408bb6e609aad7370fa36e734e8d10d5670ce20f
Giggleball <boldewijn.eli@gmail.com>
2016-10-31 00:32:14 +0100

    part 2 struggle life

--- ex7/static/includes/main.js
+++ ex7/static/includes/main.js
@@ -1,57 +1,27 @@
-$( document ).ready( function(){
-    $( '#search-box' ).keyup( function(){
+$( document ).ready( function (){
+  
+    $( '#search-box' ).keyup( function (){
         var inAr = $('#search-box').val()
-        $.post('/dex', {input: inAr}, ( data, status ) => {
-            // loop though data
-            for (var i = 0; i < data.length; i++) {   
-                // option.value = data[i]
-                $('#json-datalist').append('<option>'+ data[i].firstname + data[i].lastname + '</option>');
+        var delay = 300
+        var search_timer = null
+        $( '#search-box' ).keyup( function (e) {
+          if(search_timer) {
+            clearTimeout(search_timer)
+          }
+          search_timer = setTimeout(inAr, delay)
+                    $.post('/dex', {input: inAr}, ( data, status ) => {
+                    
+                    // loop though data
+                    for (var i = 0; i < data.length; i++) {   
+                    // option.value = data[i]
+                    $('#json-datalist').append('<option>'+ data[i].firstname + '' + data[i].lastname + '</option>');
+                    console.log( data [i] )
+                        }
+                    })
+                   });
+            })
+          })  
 
-                console.log( data [i])
-            }
-        })
-    })
-})
 
 
 
-
-
-
-
-/* isExist method two arguements
-   arr - array of values.
-   pos - string to be search.
-   */
-// Part 0
-
-// function aha( ar, pos ) {
-//   for (var i = 0; i < ar.length; i++) {
-//      if (ar[i] === 'kamal') {
-
-//          // Return index
-//          return i;         
-//      } else {        
-//          /* If value is not found return -1. */         
-//          return -1;
-//     }  
-//   }    
-// }
-
-// var power = [
-// 'sw', 'ww', 'bm'];
-
-// aha( power,'kamal' );
-
-// function getAll(arr, val) {
-//     var indexes = [], i;
-//     for(i = 0; i < arr.length; i++)
-//         if (arr[i] === val)
-//             indexes.push(i);
-//     return indexes;
-// }
-
-// var power = [
-// 'sw', 'ww', 'bm', 'ww'];
-
-// getAll( power, 'ww' )
