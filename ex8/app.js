5e46e1b6b606b59f8b91a2cd740c7593d56d15c0
Giggleball <boldewijn.eli@gmail.com>
2016-11-20 12:20:18 +0100

    inclueded routes in app.js

--- ex8/app.js
+++ ex8/app.js
@@ -27,7 +27,40 @@ app.use(session({
 
 // which visual template you'll be using 
 app.set( 'view engine', 'pug' )
+
 app.set( 'views', __dirname + '/views' )
+
+
+// Routes
+let indexRouter         = require( __dirname + '/routes/index' )
+
+let loginRouter         = require( __dirname + '/routes/login' )
+
+let logoutRouter        = require( __dirname + '/routes/logout' )
+
+let messagesRouter      = require( __dirname + '/routes/messages' )
+
+let postRouter          = require( __dirname + '/routes/post' )
+
+let profileRouter       = require( __dirname + '/routes/profile' )
+
+let registrationRouter  = require( __dirname + '/routes/reg' )
+
+
+app.use( '/router', indexRouter )
+
+app.use( '/login', loginRouter )
+
+app.use( '/messages', messagesRouter )
+
+app.use( '/logout', logoutRouter )
+
+app.use( '/post', postRouter )
+
+app.use( '/profile', profileRouter )
+
+app.use( '/register', registrationRouter )
+
 
 
 // Define database structure
@@ -88,156 +121,156 @@ app.set( 'views', __dirname + '/views' )
 // })
 
 
-// View current user + their messages and comments ^_^
-app.get('/profile', function ( req, res ) {
-    let user = req.session.user;
-    if (user === undefined) {
-        res.redirect('/')
-    } else {
-        console.log(user)
-        Promise.all([
-            User.findOne({
-                where: {
-                    id: user.id
-                }
-            }),
-            Message.findAll({
-                include: [{
-                    model: User,
-                },
-                {
-                    model: Comment,
-                    include: [User]
-                }]
-            })
-            ]).then( ( result )  => {
-                // you want to show only the messages of the current user
-                result[0].getMessages({
-                    include: [
-                        {model: User},
-                        {model: Comment, include: [User]}
-                    ]
-                }).then((messages) => {
-                    console.log(messages)
-                    res.render( 'profile', { body: messages, user: user } )
-                })
-        })       
-    }
-})
+// // View current user + their messages and comments ^_^
+// app.get('/profile', function ( req, res ) {
+//     let user = req.session.user;
+//     if (user === undefined) {
+//         res.redirect('/')
+//     } else {
+//         console.log(user)
+//         Promise.all([
+//             User.findOne({
+//                 where: {
+//                     id: user.id
+//                 }
+//             }),
+//             Message.findAll({
+//                 include: [{
+//                     model: User,
+//                 },
+//                 {
+//                     model: Comment,
+//                     include: [User]
+//                 }]
+//             })
+//             ]).then( ( result )  => {
+//                 // you want to show only the messages of the current user
+//                 result[0].getMessages({
+//                     include: [
+//                         {model: User},
+//                         {model: Comment, include: [User]}
+//                     ]
+//                 }).then((messages) => {
+//                     console.log(messages)
+//                     res.render( 'profile', { body: messages, user: user } )
+//                 })
+//         })       
+//     }
+// })
 
 
-// Logout button
-app.get( '/logout', ( req, res ) => {
-    req.session.destroy( function( error ) {
-        if( error ) {
-            throw error
-        }
-        res.redirect( '/' )
-    })
-});
+// // Logout button
+// app.get( '/logout', ( req, res ) => {
+//     req.session.destroy( function( error ) {
+//         if( error ) {
+//             throw error
+//         }
+//         res.redirect( '/' )
+//     })
+// });
 
 
-// Post page
-app.get( '/post', ( req, res ) => {
-    console.log( 'leaving a post' )
-    res.render( 'post', {
-        user: req.session.user
-    })
-})
+// // Post page
+// app.get( '/post', ( req, res ) => {
+//     console.log( 'leaving a post' )
+//     res.render( 'post', {
+//         user: req.session.user
+//     })
+// })
 
 
-// Display login form + signup form
-app.get( '/login', ( req, res ) => {
-    console.log( 'logged in' )
-    res.render( 'login', {
-        user: req.session.user
-    })
-})
+// // Display login form + signup form
+// app.get( '/login', ( req, res ) => {
+//     console.log( 'logged in' )
+//     res.render( 'login', {
+//         user: req.session.user
+//     })
+// })
 
 
-// Register
-app.post( '/register', ( req, res ) => {
-    if(req.body.name.length === 0) {
-        res.redirect('/')
-    }
+// // Register
+// app.post( '/register', ( req, res ) => {
+//     if(req.body.name.length === 0) {
+//         res.redirect('/')
+//     }
 
-    if(req.body.password.length === 0) {
-        res.redirect('/')
-    }
+//     if(req.body.password.length === 0) {
+//         res.redirect('/')
+//     }
 
-    if(req.body.email.length === 0) {
-        res.redirect('/')
-    }
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    }).then( function () {
-        res.render( 'register' )
-    })
-    console.log( 'registered  sucessfully' )
-    res.render( 'register' )
-})
+//     if(req.body.email.length === 0) {
+//         res.redirect('/')
+//     }
+//     User.create({
+//         name: req.body.name,
+//         email: req.body.email,
+//         password: req.body.password
+//     }).then( function () {
+//         res.render( 'register' )
+//     })
+//     console.log( 'registered  sucessfully' )
+//     res.render( 'register' )
+// })
 
-// After Login 
-app.post('/login', bodyParser.urlencoded({extended: true}), ( req, res ) => {
-    if(req.body.email.length === 0) {
-        res.redirect('/')
-    }
+// // After Login 
+// app.post('/login', bodyParser.urlencoded({extended: true}), ( req, res ) => {
+//     if(req.body.email.length === 0) {
+//         res.redirect('/')
+//     }
 
-    if(req.body.password.length === 0) {
-        res.redirect('/')
-    }
+//     if(req.body.password.length === 0) {
+//         res.redirect('/')
+//     }
 
-    User.findOne({
-        where: {
-            email: req.body.email
-        }
-    }).then( function ( user ) {
-        if ( user !== null && req.body.password === user.password ) {
-            req.session.user = user 
-            res.redirect('/messages')
-        } else {
-            res.redirect('/')
-        }
-    })
-})
-
-
-// Posting to the messageboard
-app.post( '/post', ( req, res ) => {
-    console.log(req.session.user)
-    User.findOne({
-        where: { name: req.session.user.name },
-        order: '"createdAt" DESC'
-    }).then( ( thisuser ) => {
-        console.log(thisuser)
-        thisuser.createMessage({
-            title: req.body.title,
-            body: req.body.body
-        }).then( () => {
-            res.redirect( '/messages' )
-        })
-    })
-})
+//     User.findOne({
+//         where: {
+//             email: req.body.email
+//         }
+//     }).then( function ( user ) {
+//         if ( user !== null && req.body.password === user.password ) {
+//             req.session.user = user 
+//             res.redirect('/messages')
+//         } else {
+//             res.redirect('/')
+//         }
+//     })
+// })
 
 
-// Commenting to the messageboard
-app.post( '/messages', ( req, res ) => {
-    console.log(req.session.user)
-    User.findOne({
-        where: { name: req.session.user.name },
-        order: '"createdAt" DESC'
-    }).then( (thisuser) => {
-        console.log(thisuser)
-        thisuser.createComment({
-            body: req.body.body,
-            messageId: req.body.post_id
-        }).then( () => {
-            res.redirect( '/messages' )
-        })
-    })
-})
+// // Posting to the messageboard
+// app.post( '/post', ( req, res ) => {
+//     console.log(req.session.user)
+//     User.findOne({
+//         where: { name: req.session.user.name },
+//         order: '"createdAt" DESC'
+//     }).then( ( thisuser ) => {
+//         console.log(thisuser)
+//         thisuser.createMessage({
+//             title: req.body.title,
+//             body: req.body.body
+//         }).then( () => {
+//             res.redirect( '/messages' )
+//         })
+//     })
+// })
+
+
+// // Commenting to the messageboard
+// app.post( '/messages', ( req, res ) => {
+//     console.log(req.session.user)
+//     User.findOne({
+//         where: { name: req.session.user.name },
+//         order: '"createdAt" DESC'
+//     }).then( (thisuser) => {
+//         console.log(thisuser)
+//         thisuser.createComment({
+//             body: req.body.body,
+//             messageId: req.body.post_id
+//         }).then( () => {
+//             res.redirect( '/messages' )
+//         })
+//     })
+// })
 
 
 // Sync database
