18785d149713206e2201c0ec7bc5f3c482ba4e84
Giggleball <boldewijn.eli@gmail.com>
2016-12-15 19:49:38 +0100

    brad hussey

--- Game/public/js/custom.js
+++ Game/public/js/custom.js
@@ -32,10 +32,10 @@ $(document).ready(function () {
 
 
 	$('#my-memorygame').quizyMemoryGame( {
-		itemWidth: 98, itemHeight: 98, itemsMargin:40, colCount:5, animType:'flip', flipAnim:'tb', animSpeed:180, resultIcons:true});
+		itemWidth: 75, itemHeight: 75, itemsMargin:25, colCount:5, animType:'flip', flipAnim:'tb', animSpeed:140, resultIcons:true});
 	
 	$('#my-memorygame_ra').quizyMemoryGame( {
-		itemWidth: 98, itemHeight: 98, itemsMargin:40, colCount:4, animType:'flip', flipAnim:'tb', animSpeed:180, resultIcons:true});
+		itemWidth: 75, itemHeight: 75, itemsMargin:25, colCount:4, animType:'flip', flipAnim:'tb', animSpeed:140, resultIcons:true});
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 							////////////////// MeM0Bo@rD //////////////////
